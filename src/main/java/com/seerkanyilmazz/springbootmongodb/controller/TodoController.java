c33c0c0808f5ded5e4d99cf480954dc192fb8a08
seerkanyilmazz <seerkanyilmazz@gmail.com>
2021-08-17 22:48:04 +0300

    Unnecessary import deleted

--- src/main/java/com/seerkanyilmazz/springbootmongodb/controller/TodoController.java
+++ src/main/java/com/seerkanyilmazz/springbootmongodb/controller/TodoController.java
@@ -4,7 +4,6 @@ import com.seerkanyilmazz.springbootmongodb.Repository.ITodoRepository;
 import com.seerkanyilmazz.springbootmongodb.exception.TodoCollectionException;
 import com.seerkanyilmazz.springbootmongodb.model.TodoDTO;
 import com.seerkanyilmazz.springbootmongodb.service.ITodoService;
-import com.seerkanyilmazz.springbootmongodb.service.TodoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
